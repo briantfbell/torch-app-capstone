31e887f613342ce913bb3b6e1be610f529ce977d
KairosWithJ <azemopoulosjared@gmail.com>
2026-04-06 15:15:54 -0600

    should not nuke main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import MainRouter from './services/Router'
-import './App.css';
-import MiniDrawer from './components/ui/MiniDrawer.jsx';
-import InventoryTable from './InventoryTable';
+import "./App.css";
+import MainRouter from "./services/Router";
+import "./App.css";
+import MiniDrawer from "./components/ui/MiniDrawer.jsx";
+import InventoryTable from "./InventoryTable";
 
 function App() {
-    return (
-        <MiniDrawer>
-            <MainRouter/>
-            <InventoryTable/>
-        </MiniDrawer>
-
-
-    );
+  return (
+    <MiniDrawer>
+      <MainRouter />
+      <InventoryTable />
+    </MiniDrawer>
+  );
 }
 
 export default App;
