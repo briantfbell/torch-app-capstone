a5af89374c3aec974ce0bbebda375ff7fc92a6b8
keenanmacneill <macneill.keenan@gmail.com>
2026-04-06 14:06:59 -0400

    finishes auth routes, needs testing with db connections

--- backend/api/models/authModels.js
+++ backend/api/models/authModels.js
@@ -6,7 +6,24 @@ const baseQuery = () =>
     .join('ranks', 'users.rank', 'ranks.rank')
     .join('uic', 'users.uic_id', 'uic.uic')
     .join('users_roles', 'users.id', 'users_roles.user_id')
-    .join('roles', 'users_roles.role_id', 'roles.id');
+    .join('roles', 'users_roles.role_id', 'roles.id')
+    .select(
+      'users.id',
+      'users.username',
+      'users.name_first',
+      'users.name_last',
+      'users.email',
+      'users.phone',
+      'users.created_at',
+      'users.updated_at',
+      'ranks.rank',
+      'uic.uic',
+      'roles.id as role_id',
+      'roles.admin',
+      'roles.hand_receipt_holder',
+      'roles.sub_receipt_holder',
+      'roles.temp_receipt_holder',
+    );
 
 // TO DO: can users have multiple roles?
 
@@ -17,32 +34,61 @@ const baseQuery = () =>
 //     .groupBy('users.id');
 // };
 
-exports.createUser = async user => {
-  return await baseQuery()
-    .insert(user)
-    .returning([
-      'id',
-      'role',
-      'username',
-      'name_first',
-      'name_last',
-      'email',
-      'phone',
-      'created_at',
-      'updated_at',
-      'rank',
-      'uic',
-    ]);
+exports.createUser = async (user, roleId) => {
+  return await db.transaction(async trx => {
+    const [newUser] = await trx('users')
+      .insert({
+        username: user.username,
+        name_first: user.name_first,
+        name_last: user.name_last,
+        email: user.email,
+        password: user.password,
+        phone: user.phone,
+        rank: user.rank,
+        uic_id: user.uic_id,
+      })
+      .returning('id');
+
+    await trx('users_roles').insert({
+      user_id: newUser.id,
+      role_id: roleId,
+    });
+
+    return await trx('users')
+      .join('ranks', 'users.rank', 'ranks.rank')
+      .join('uic', 'users.uic_id', 'uic.uic')
+      .join('users_roles', 'users.id', 'users_roles.user_id')
+      .join('roles', 'users_roles.role_id', 'roles.id')
+      .where('users.id', newUser.id)
+      .select(
+        'users.id',
+        'users.username',
+        'users.name_first',
+        'users.name_last',
+        'users.email',
+        'users.phone',
+        'users.created_at',
+        'users.updated_at',
+        'ranks.rank',
+        'uic.uic',
+        'roles.id as role_id',
+        'roles.admin',
+        'roles.hand_receipt_holder',
+        'roles.sub_receipt_holder',
+        'roles.temp_receipt_holder',
+      )
+      .first();
+  });
 };
 
 exports.findUserById = async id => {
-  return await baseQuery().where({ id: id }).first();
+  return await baseQuery().where('users.id', id).first();
 };
 
 exports.findUserByEmail = async email => {
-  return await baseQuery().select('*').where('email', email).first();
+  return await baseQuery().where('email', email).first();
 };
 
 exports.findUserByUsername = async username => {
-  return await baseQuery().select('*').where('username', username).first();
+  return await baseQuery().where('username', username).first();
 };
