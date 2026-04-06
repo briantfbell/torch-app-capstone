c16bf350f71c4a3188d768b7021a23d611f229c7
Benjamin <ben.bao.tran@gmail.com>
2026-04-06 15:18:54 -0400

    Merge me  (#131)
    * Splash page completed, routing added, first commit (#128)
    
    Co-authored-by: Hyliangoat <reignoflife@gmail.com>
    
    * view-shr-pdf: Added button that takes you to SHR viewer page + nav controls (hard-coded to example SHR pdf) (#127)
    
    Co-authored-by: Dylan Rivera Luciano <37868283+drluciano@users.noreply.github.com>
    
    * Fixed navbar: (#129)
    
    G2G
    
    ---------
    
    Co-authored-by: Dylan Rivera Luciano <37868283+drluciano@users.noreply.github.com>
    Co-authored-by: Hyliangoat <reignoflife@gmail.com>
    Co-authored-by: Brian Bell <61871590+briantfbell@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
+import MainRouter from './services/Router';
 import MiniDrawer from './components/ui/MiniDrawer.jsx';
 
 function App() {
+    return (
+        <MiniDrawer>
+            <MainRouter/>
+        </MiniDrawer>
 
-  return (
-    <>
-        <MiniDrawer/>
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
