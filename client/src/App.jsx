fbb1ae3ab136c62dd8eee18385b747e9f155022c
Dylan Rivera Luciano <37868283+drluciano@users.noreply.github.com>
2026-04-06 15:14:31 -0400

    Fixed navbar: (#129)
    G2G

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EquipmentPage from "./pages/EquipmentPage";
-import SHRViewPage from "./pages/SHRViewPage";
-import { useState } from 'react'
 import './App.css';
-import MainRouter from './services/Router'
+import MainRouter from './services/Router';
 import MiniDrawer from './components/ui/MiniDrawer.jsx';
 
 function App() {
-  return (
-    <MainRouter />
-    <MiniDrawer/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<EquipmentPage />} />
-        <Route path="/shr-viewer" element={<SHRViewPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <MiniDrawer>
+            <MainRouter/>
+        </MiniDrawer>
+
+    );
 }
 
 export default App;
