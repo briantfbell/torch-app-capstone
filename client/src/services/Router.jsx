def5c8e7c71b891b36346a6e8e8507f3be720a97
KairosWithJ <azemopoulosjared@gmail.com>
2026-04-06 15:20:54 -0600

    should not nuke main (#140)

--- client/src/services/Router.jsx
+++ client/src/services/Router.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import SplashPage from '../pages/SplashPage';
-import EquipmentPage from '../pages/EquipmentPage.jsx';
-import SHRViewPage from '../pages/SHRViewPage.jsx';
-import EndItemPage from '../pages/EndItemPage.jsx';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SplashPage from "../pages/SplashPage";
+import EquipmentPage from "../pages/EquipmentPage.jsx";
+import SHRViewPage from "../pages/SHRViewPage.jsx";
+import EndItemPage from "../pages/EndItemPage.jsx";
 
 export default function MainRouter() {
-    return(
-        <Router>
-            <Routes>
-                <Route path = '/' element={<SplashPage />} />
-                <Route path="/" element={<EquipmentPage />} />
-                <Route path="/shr-viewer" element={<SHRViewPage />} />
-                <Route path="/enditem/:id" element={<EndItemPage />} />
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SplashPage />} />
+        <Route path="/equipment" element={<EquipmentPage />} />
+        <Route path="/shr-viewer" element={<SHRViewPage />} />
+        <Route path="/enditem/:id" element={<EndItemPage />} />
+      </Routes>
+    </Router>
+  );
 }
