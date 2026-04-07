00d3c25f078b3acf17395eccf24ba3f1d0c0ff41
Hyliangoat <reignoflife@gmail.com>
2026-04-07 12:31:53 -0400

    Registration complete, clear form and user array added as well

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import Button from '@mui/material/Button'
+import { Checkbox, FormGroup, RadioGroup, FormControlLabel, FormLabel, FormControl, Stack, TextField, Radio } from "@mui/material"
+
 export default function RegisterForm({onSubmit, error}){
     //Form for form data (trying something new)
-    const [form, setForm] = useState( {
+
+    const initialFormState = {
         username: '',
         password: '',
         confirmPass: '',
@@ -11,11 +14,49 @@ export default function RegisterForm({onSubmit, error}){
         name_last: '',
         phone: '',
         rank: '',
-        role: '',
         DoDID: '',
         uic: ''
-    })
+    }
+    const [form, setForm] = useState(initialFormState)
 
+
+    //clearing the screen
+    const handleClear = () => {
+        if(!window.confirm('Clear entire form?')) return;
+        setForm(initialFormState)
+        setRoles([])
+        setAccountType('user');
+        setLocalError('');
+    }
+
+    //role modification
+    const [accountType, setAccountType] = useState('user'); 
+    const [roles, setRoles] = useState([]);
+
+    const handleAccountTypeChange = (e) => {
+        const value = e.target.value;
+        setAccountType(value)
+
+        if (value === 'admin'){
+            setRoles(['admin'])
+        } else {
+            setRoles(['user'])
+        }
+    }
+
+    const handleRoleChange = (e) => {
+        const {value, checked} = e.target;
+
+        if (accountType === 'admin') return;
+
+        setRoles((prev) => {
+            if(checked) {
+                return [...prev, value]
+            } else {
+                return prev.filter((r) => r !== value)
+            }
+        })
+    }
     //Error messaging
     const [localError, setLocalError] = useState('')
 
@@ -29,14 +70,14 @@ export default function RegisterForm({onSubmit, error}){
         })
 
         //When change happens, clear error
-        setError('');
+        setLocalError('');
     }
     const handleSubmit = (e) => {
         e.preventDefault();
 
         //Validate all filled
         const {confirmPass, ...rest} = form;
-        if(Object.values(rest).some(v => !v)){
+        if(Object.values(rest).some((v) => !v)){
             return setLocalError('All fields are required!')
         }
 
@@ -50,12 +91,12 @@ export default function RegisterForm({onSubmit, error}){
             username: form.username.trim().toLowerCase(), //trim it for spaces, lowercase it for safety
             password: form.password,
             uic: form.uic.trim(),
-            email: form.email.trim().toLowerCase,
+            email: form.email.trim().toLowerCase(),
             name_first: form.name_first.trim(),
             name_last: form.name_last.trim(),
             phone: form.phone.trim(),
             rank: form.rank.trim(),
-            role: form.role,
+            role: roles,
             DoDID: form.DoDID.trim()
         }
 
@@ -64,54 +105,69 @@ export default function RegisterForm({onSubmit, error}){
 
     return(
         <form className='registerFormContainer' onSubmit={handleSubmit}>
-            <div className='mainRegisterForm'>
-                <input onChange={handleChange} name='username' type='text' placeholder="Username"/>
-                <input onChange={handleChange} name='password' type='password' placeholder="Password"/>
-                <input onChange={handleChange} name='confirmPassword' type='password' placeholder="Confirm Password"/>
+            <Stack spacing={2} direction="row" justifyContent="center">
+                <Stack spacing={2}>
+                <TextField id='outlined-basic'  value={form.username} label='username' required onChange={handleChange} name='username' type='text' placeholder="Username"/>
+                <TextField id='outlined-basic'  value={form.password} label='password' required onChange={handleChange} name='password' type='password' placeholder="Password"/>
+                <TextField id='outlined-basic'  value={form.confirmPass} label='confirmPass' required onChange={handleChange} name='confirmPass' type='password' placeholder="Confirm Password"/>
                 {/* etc info, TODO: refactor for visualization */}
-                <input 
+                <TextField id='outlined-basic'  label='email' required value={form.email}
                     onChange={handleChange}
                     name='email'
                     placeholder="Email Address"
                 />
-                <input 
+                <TextField id='outlined-basic'  label='name_first' required value={form.name_first}
                     onChange={handleChange}
                     name='name_first'
                     placeholder="First Name"
                 />
-                <input 
+                <TextField id='outlined-basic'  label='name_last' required value={form.name_last}
                     onChange={handleChange}
                     name='name_last'
                     placeholder="Last Name"
                 />
-                <input 
+                <TextField id='outlined-basic'  label='phone' required value={form.phone}
                     onChange={handleChange}
                     name='phone'
                     placeholder="Phone Number"
                 />
-                <input 
+                </Stack>
+                <Stack spacing={2}>
+                <TextField id='outlined-basic'  label='rank' required value={form.rank}
                     onChange={handleChange}
                     name='rank'
                     placeholder="Rank"
                 />
-                <input 
-                    onChange={handleChange}
-                    name='role'
-                    placeholder="Role (Command, supply, etc)"
-                />
-                <input 
+                <FormControl required>
+                    <FormLabel id="role">Account Type</FormLabel>
+                    <RadioGroup value={accountType} onChange={handleAccountTypeChange}>
+                        <FormControlLabel value="admin" control={<Radio />} label="Admin" />
+                        <FormControlLabel value="user" control={<Radio />} label="User" />
+                    </RadioGroup>
+                    <FormLabel>Roles</FormLabel>
+                    <FormGroup required>
+                        <FormControlLabel control={<Checkbox value= 'hrh' checked={roles.includes('hrh')} disabled={accountType === 'admin'} onChange={handleRoleChange} />} label="HRH" />
+                        <FormControlLabel control={<Checkbox value = 'sub-hrh' checked={roles.includes('sub-hrh')} disabled={accountType === 'admin'} onChange={handleRoleChange} />} label="sub-HRH" />
+                        <FormControlLabel control={<Checkbox value = 't-hrh' checked={roles.includes('t-hrh')} disabled={accountType === 'admin'} onChange={handleRoleChange}/>} label="t-HRH" />
+                    </FormGroup>
+                </FormControl>
+                <TextField id='outlined-basic'  label='DoDID' required value={form.DoDID}
                     onChange={handleChange}
                     name='DoDID'
                     placeholder="DoDID"
                 />
-                <input 
+                <TextField id='outlined-basic'  label='uic' required value={form.uic}
                     onChange={handleChange}
-                    name='UIC'
+                    name='uic'
                     placeholder="Unit UIC"
                 />
-            </div>
-
-            <Button type="submit" variant="contained">Register</Button>
+                </Stack>
+            </Stack>
+            <br/>
+            <Stack direction = "row" spacing={2} justifyContent="center">
+                <Button type="submit" variant="contained">Register</Button>
+                <Button type="button" variant="outlined" color="secondary" onClick={handleClear}>Clear Form</Button>
+            </Stack>
 
             {(localError || error)}
         </form>
