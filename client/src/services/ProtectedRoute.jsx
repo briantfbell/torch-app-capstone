41df94921e222bf2456dfe65fe8e91eec7e3a0f6
Hyliangoat <reignoflife@gmail.com>
2026-04-07 16:07:33 -0400

    Added a commented desireable in Router.jsx

--- client/src/services/ProtectedRoute.jsx
+++ client/src/services/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom"
 import {useAuth} from '../hooks/useAuth'
-
+// import {Navigate} from 'react-navigation/native'
 export default function ProtectedRoute({children}) {
     const {user, loading} = useAuth();
 
@@ -12,3 +12,18 @@ export default function ProtectedRoute({children}) {
 
     return children;
 }
+
+    // if(!user) {
+    //     Alert.alert(
+    //         'Authentication Failed',
+    //         'Please log in and try again',
+    //         [
+    //             {
+    //                 text: 'OK',
+    //                 onPress: () => {<Navigate to="/" replace />}
+    //             },
+    //         ],
+    //         {cancelable: false}
+    //     )
+    // }
+    //Tryna use this if we can pretty please import some react-navigation
