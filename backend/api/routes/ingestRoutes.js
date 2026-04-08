3dc925108abdfaddcf3f72f478e1cfe9ffffc553
keenanmacneill <macneill.keenan@gmail.com>
2026-04-08 16:59:24 -0400

    working xslx parse

--- backend/api/routes/ingestRoutes.js
+++ backend/api/routes/ingestRoutes.js
@@ -1,28 +1,88 @@
 const express = require('express');
-const auth = require('../middleware/auth'); //Use after testing
 const multer = require('multer');
-const readXlsxFile = require('read-excel-file/node');
-
-const app = express();
+const { readSheet, parseData } = require('read-excel-file/node');
+const auth = require('../middleware/auth');
 
 const upload = multer({ storage: multer.memoryStorage() });
-
 const router = express.Router();
 
-router.post('/excel', upload.single('file'), (req, res) => {
+router.post('/excel', upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
+  try {
+    const data = await readSheet(req.file.buffer);
 
-  readXlsxFile(req.file.buffer)
-    .then(rows => {
-      console.table(rows);
-      console.table(rows[0].data);
-      res.json({ data: rows });
-    })
-    .catch(err => {
-      res.status(500).send('Error parsing file: ' + err.message);
-    });
+    const schema = {
+      lin: {
+        column: 'LIN Number / DODIC',
+        type: String,
+      },
+      fsc: {
+        column: 'FSC',
+        type: Number,
+        required: true,
+      },
+      description: {
+        column: 'Material Description',
+        type: String,
+        required: true,
+      },
+      auth_qty: {
+        column: 'Stock',
+        type: Number,
+        required: true,
+      },
+      ui: {
+        column: 'Unit of Measure',
+        type: String,
+        required: true,
+      },
+      // serial_number: {
+      //   column: 'Serial Number',
+      //   type: Number,
+      //   required: true,
+      // },
+    };
+
+    const results = parseData(data, schema);
+
+    const errors = [];
+    const objects = [];
+
+    let row = 1;
+
+    for (const { errors: errorsInRow, object } of results) {
+      if (errorsInRow) {
+        for (const error of errorsInRow) {
+          errors.push({ error, row });
+        }
+      } else {
+        objects.push(object);
+      }
+      row++;
+    }
+
+    if (errors.length > 0) {
+      for (const { error, row } of errors) {
+        console.error(
+          'Error in data row',
+          row,
+          'column',
+          error.column,
+          ':',
+          error.error,
+          error.reason || '',
+        );
+      }
+    } else {
+      console.log('Objects', objects);
+    }
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .send('Error parsing Excel file: ' + err.message);
+  }
 });
 
 module.exports = router;
