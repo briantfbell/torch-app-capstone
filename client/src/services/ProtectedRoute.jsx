48e30ef5fac599c91ad7d10665a73ae12e48f841
Hyliangoat <reignoflife@gmail.com>
2026-04-08 11:19:16 -0400

    working alert on redirect

--- client/src/services/ProtectedRoute.jsx
+++ client/src/services/ProtectedRoute.jsx
@@ -1,29 +1,14 @@
 import { Navigate } from "react-router-dom"
 import {useAuth} from '../hooks/useAuth'
-// import {Navigate} from 'react-navigation/native'
 export default function ProtectedRoute({children}) {
     const {user, loading} = useAuth();
 
     if (loading) return <div>Authenticating...</div>
 
     if(!user) {
+        window.alert("Authentication failed, log in and try again")
         return <Navigate to="/" replace />
     }
 
     return children;
 }
-
-    // if(!user) {
-    //     Alert.alert(
-    //         'Authentication Failed',
-    //         'Please log in and try again',
-    //         [
-    //             {
-    //                 text: 'OK',
-    //                 onPress: () => {<Navigate to="/" replace />}
-    //             },
-    //         ],
-    //         {cancelable: false}
-    //     )
-    // }
-    //Tryna use this if we can pretty please import some react-navigation
