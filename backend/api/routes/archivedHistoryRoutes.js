8d3aa3bcbff6e59078811b8e19d2467b05c39558
keenanmacneill <macneill.keenan@gmail.com>
2026-04-09 17:16:11 -0400

    comments out delete and update routes for archive

--- backend/api/routes/archivedHistoryRoutes.js
+++ backend/api/routes/archivedHistoryRoutes.js
@@ -7,14 +7,14 @@ const {
   getArchivedHistoryById,
   getArchivedHistory,
   createArchivedHistory,
-  updateArchivedHistory,
-  deleteArchivedHistory,
+  // updateArchivedHistory,
+  // deleteArchivedHistory,
 } = require('../controllers/archivedHistoryControllers');
 
 router.get('/:id', auth, getArchivedHistoryById);
 router.get('/', auth, getArchivedHistory);
 router.post('/', auth, createArchivedHistory);
-router.patch('/:id', auth, updateArchivedHistory);
-router.delete('/:id', auth, deleteArchivedHistory);
+// router.patch('/:id', auth, updateArchivedHistory);
+// router.delete('/:id', auth, deleteArchivedHistory);
 
 module.exports = router;
