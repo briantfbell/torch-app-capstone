6ae756fb8a6e222f6185081db5b05a95821bc9ea
Benjamin Tran <ben.bao.tran@gmail.com>
2026-04-09 12:53:16 -0400

    enditempage completed.
    # Conflicts:
    #	client/src/pages/EndItemPage.jsx
    
    # Conflicts:
    #	backend/api/routes/endItemsRoutes.js

--- backend/api/routes/endItemsRoutes.js
+++ backend/api/routes/endItemsRoutes.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const adminAuth = require('../middleware/adminAuth');
-const hrhAuth = require('../middleware/hrhAuth');
+// const auth = require('../middleware/auth');
+// const adminAuth = require('../middleware/adminAuth');
 
 const router = express.Router();
 
 const {
-  getEndItemsByUicId,
   getEndItemById,
   getAllEndItems,
   createEndItem,
   updateEndItem,
   deleteEndItem,
-  markEndItemComplete,
 } = require('../controllers/endItemsControllers');
 
-router.get('/uic/:uic_id', getEndItemsByUicId);
 router.get('/:id', getEndItemById);
 router.get('/', getAllEndItems);
-router.post('/', hrhAuth, createEndItem);
-router.patch('/:id/complete', hrhAuth, markEndItemComplete);
-router.patch('/:id', hrhAuth, updateEndItem);
-router.delete('/:id', hrhAuth, deleteEndItem);
+router.post('/', createEndItem);
+router.patch('/:id', updateEndItem);
+router.delete('/:id', deleteEndItem);
 
 module.exports = router;
