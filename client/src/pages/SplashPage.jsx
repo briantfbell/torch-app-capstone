86e992fff8fc271cc05273261bbdc7de61006d6a
Hyliangoat <reignoflife@gmail.com>
2026-04-09 12:06:16 -0400

    More merge nonsense

--- client/src/pages/SplashPage.jsx
+++ client/src/pages/SplashPage.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-=======
-import { Stack } from '@mui/material';
->>>>>>> origin/main
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
