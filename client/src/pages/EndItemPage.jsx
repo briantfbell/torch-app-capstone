c6272212032cc9f7ea0effe73cc71989ae92eeaf
Benjamin Tran <ben.bao.tran@gmail.com>
2026-04-10 14:31:33 -0400

    updates

--- client/src/pages/EndItemPage.jsx
+++ client/src/pages/EndItemPage.jsx
@@ -180,6 +180,7 @@ export default function EndItemPage() {
                           {endItem.auth_qty}
                         </Typography>
                       </CardContent>
+                    </Card>
                   </Stack>
                 </Stack>
 
