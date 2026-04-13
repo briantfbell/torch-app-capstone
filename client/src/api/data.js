ea355eb9c06480a811dd3b9f62742b7e6d342135
Dylan Rivera <dylriv@icloud.com>
2026-04-13 14:00:50 -0400

    Dashboard MVP done

--- client/src/api/data.js
+++ client/src/api/data.js
@@ -1,10 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
 
+
+//Get all
+
 export const tryGetEndItems = async () => {
     try {
         const res = await fetch(`${API_URL}/end-items`, {
             method: "GET",
-            headers: { "Content-type": "application/json; charset=UTF-8" },
+            headers: {"Content-type": "application/json; charset=UTF-8"},
             credentials: 'include'
         });
         const data = await res.json();
@@ -16,7 +19,7 @@ export const tryGetEndItems = async () => {
         return data;
     } catch (e) {
         console.error(e);
-        return { message: "Unable to reach the server." };
+        return {message: "Unable to reach the server."};
     }
 };
 
@@ -24,7 +27,7 @@ export const tryGetSerialItems = async () => {
     try {
         const res = await fetch(`${API_URL}/serial-items`, {
             method: "GET",
-            headers: { "Content-type": "application/json; charset=UTF-8" },
+            headers: {"Content-type": "application/json; charset=UTF-8"},
             credentials: 'include'
         });
         const data = await res.json();
@@ -36,6 +39,112 @@ export const tryGetSerialItems = async () => {
         return data;
     } catch (e) {
         console.error(e);
-        return { message: "Unable to reach the server." };
+        return {message: "Unable to reach the server."};
+    }
+};
+
+export const tryGetComponents = async () => {
+    try {
+        const res = await fetch(`${API_URL}/components`, {
+            method: "GET",
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data;
+        }
+
+        return data;
+    } catch (e) {
+        console.error(e);
+        return {message: "Unable to reach the server."};
+    }
+};
+
+
+//Component current history and archived
+
+export const tryGetHistoryComponent = async () => {
+    try {
+        const res = await fetch(`${API_URL}/current-history/components`, {
+            method: "GET",
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data;
+        }
+
+        return data;
+    } catch (e) {
+        console.error(e);
+        return {message: "Unable to reach the server."};
+    }
+};
+
+export const tryGetHistoryComponentArchive = async () => {
+    try {
+        const res = await fetch(`${API_URL}/archived-history/components`, {
+            method: "GET",
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data;
+        }
+
+        return data;
+    } catch (e) {
+        console.error(e);
+        return {message: "Unable to reach the server."};
+    }
+};
+
+
+//End items current history and archived
+
+export const tryGetHistoryEndItems = async () => {
+    try {
+        const res = await fetch(`${API_URL}/current-history/end-items`, {
+            method: "GET",
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data;
+        }
+
+        return data;
+    } catch (e) {
+        console.error(e);
+        return {message: "Unable to reach the server."};
+    }
+};
+
+export const tryGetHistoryEndItemArchive = async () => {
+    try {
+        const res = await fetch(`${API_URL}/archived-history/end-items`, {
+            method: "GET",
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data;
+        }
+
+        return data;
+    } catch (e) {
+        console.error(e);
+        return {message: "Unable to reach the server."};
     }
 };
